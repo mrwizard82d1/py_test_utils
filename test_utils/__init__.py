f491bfac97a8bd0c2e4854ebc36bdd65e092faa8
Larry Jones <larry.jones@reveal-energy.com>
2020-11-30 10:44:56 -0600

    Write rand_digit, rand_2 through rand_9

---
+++ test_utils/__init__.py
@@ -0,0 +1,15 @@
+#
+# This file is part of Orchid and related technologies.
+#
+# Copyright (c) 2017-2020 Reveal Energy Services.  All Rights Reserved.
+#
+# LEGAL NOTICE:
+# Orchid contains trade secrets and otherwise confidential information
+# owned by Reveal Energy Services. Access to and use of this information is 
+# strictly limited and controlled by the Company. This file may not be copied,
+# distributed, or otherwise disclosed outside of the Company's facilities 
+# except under appropriate precautions to maintain the confidentiality hereof, 
+# and may not be used in any way not expressly authorized by the Company.
+#
+
+from .core import rand_digit, rand_2, rand_3, rand_4, rand_5, rand_6, rand_7, rand_8, rand_9
---
+++ test_utils/core.py
@@ -0,0 +1,51 @@
+#
+# This file is part of Orchid and related technologies.
+#
+# Copyright (c) 2017-2020 Reveal Energy Services.  All Rights Reserved.
+#
+# LEGAL NOTICE:
+# Orchid contains trade secrets and otherwise confidential information
+# owned by Reveal Energy Services. Access to and use of this information is 
+# strictly limited and controlled by the Company. This file may not be copied,
+# distributed, or otherwise disclosed outside of the Company's facilities 
+# except under appropriate precautions to maintain the confidentiality hereof, 
+# and may not be used in any way not expressly authorized by the Company.
+#
+
+import random
+
+
+def rand_digit():
+    return random.randrange(10)
+
+
+def rand_2():
+    return random.randrange(100)
+
+
+def rand_3():
+    return random.randrange(1000)
+
+
+def rand_4():
+    return random.randrange(10000)
+
+
+def rand_5():
+    return random.randrange(100000)
+
+
+def rand_6():
+    return random.randrange(1000000)
+
+
+def rand_7():
+    return random.randrange(10000000)
+
+
+def rand_8():
+    return random.randrange(100000000)
+
+
+def rand_9():
+    return random.randrange(1000000000)
